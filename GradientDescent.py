f314ef08b4ae1fc9c1f75268b23ca02dffe3d6ab
Jeyanth <66862827+jeyanth-jr@users.noreply.github.com>
2022-10-04 20:04:24 +0530

    Initial commit

---
+++ GradientDescent.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.random.randn(10,1)
+y=2*x+np.random.rand()
+w=0.00
+b=0.00
+
+def cost_function(w,b,x,y):
+    m=len(x)
+    cost=0
+    for i in range(m):
+        y_pred=w+b*x[i]
+        cost+=(y_pred-y[i])**2
+    return cost
+
+def cost_gradient(w,b,x,y):
+    m=len(x)
+    dj_dw=0
+    dj_db=0
+    for i in range(m):
+        dj_dw+=(w+b*x[i])-y[i]
+        dj_db+=((w+b*x[i])-y[i])*x[i]
+    dj_dw=dj_dw/m
+    dj_db=dj_db/m
+    return dj_dw,dj_db
+cost=[]
+def gradientdescent(w,b,x,y,iters,alpha):
+    for i in range(iters):
+        dj_dw,dj_db=cost_gradient(w,b,x,y)
+        temp_w=w-(alpha*dj_dw)
+        temp_b=b-(alpha*dj_db)
+        w=temp_w
+        b=temp_b
+        print(cost_function(w,b,x,y),"w=",w,"b=",b)
+        cost.append(cost_function(w,b,x,y))
+    return w,b
+iters=[x for x in range(500)]    
+w,b=gradientdescent(w,b,x,y,500,0.01)
+plt.scatter(x,y)
+plt.plot(b*x+w,y)
+plt.show()
+plt.plot(iters,cost)
+plt.xlabel("iters")
+plt.ylabel("cost")
+plt.show()
